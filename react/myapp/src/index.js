e8732427927cd8af9cb035b6b15b22cd4520fbfa
vikram87in <vikram87in@gmail.com>
2019-09-15 22:37:14 +0530

    updated

--- react/myapp/src/index.js
+++ react/myapp/src/index.js
@@ -2,18 +2,64 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class App extends Component {
+class Clock extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date(),
+            counter: 0
+        }
+    }
+
+    componentDidMount() {
+        // setInterval(() => {
+        //     this.tick();
+        // }, 1000);
+    }
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     return false;
+    // }
+
+    onButtonClick = (one,two) => {
+        debugger;
+        this.setState((state, props) => {
+            return { counter: state.counter + 1 }
+        });
+        this.setState((state, props) => {
+            return { counter: state.counter + 1 }
+        });
+        // this.setState((state,props)=>{
+        //     return {counter: state.counter+1}
+        // });
+        // this.setState({counter: this.state.counter+1});
+        // this.setState({counter: this.state.counter+1});
+        // this.setState({counter: this.state.counter+1});
+
+        // this.setState((prevState) => ({ counter: prevState.counter + 1 }))
+        // this.setState((prevState) => ({ counter: prevState.counter + 1 }))
+        // this.setState((prevState) => ({ counter: prevState.counter + 1 }))
+    }
+
+
+    tick() {
+        this.setState({ date: new Date() });
+    }
+
     render() {
+        let number = 100;
         return (
-            <div className="App">
-                <h1>Hello, React!</h1>
-                <h2>It is {new Date().toLocaleTimeString()}.</h2>
-                <button>hi</button>
-            </div>
-        )
+            <div>
+                <h1>Hello, world! The counter is {this.state.counter}</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+                <button onClick={this.onButtonClick.bind(this)}>+3</button>
+                <button onClick={this.onButtonClick.bind(this)}>+3</button>
+            </div >
+        );
     }
 }
-setInterval(() => {
-    ReactDOM.render(<App />, document.getElementById('root'));
-}, 1000);
+
+ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+);
 
